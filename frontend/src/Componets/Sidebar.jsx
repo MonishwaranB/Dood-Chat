f5ec2668cca13ded67d124afd66af0f2fc478e4a
MonishwaranB <monishwaran1504@gmail.com>
2024-08-01 17:44:25 +0530

    test 2

--- frontend/src/Componets/Sidebar.jsx
+++ frontend/src/Componets/Sidebar.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './MyStyles.css';
+import AccountCircleTwoToneIcon from '@mui/icons-material/AccountCircleTwoTone';
+import { IconButton } from '@mui/material';
 
 function Sidebar() {
   return (
-    <div className='sidebar-container'>Sidebar</div>
+    <div className='sidebar-container'>
+      <div className='sb-header'>
+        <IconButton><AccountCircleTwoToneIcon/></IconButton>   
+      </div>
+      <div className='sb-search'>search</div>
+      <div className='sb-conversations'>conver</div>
+    </div>
   )
 }
 
